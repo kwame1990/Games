cbc474cc9de008ad4e466767ba358bb5aa5b6b73
kwame1990 <361390102@qq.com>
2014-04-18 13:21:47 +0800

    first commit

---
+++ remove/lx2/lx2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  lx2
+//
+//  Created by teluor on 14-2-23.
+//  Copyright (c) 2014年 teluor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
